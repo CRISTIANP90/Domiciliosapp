b627badc9aebaffe6e1dcc67d9550d9dd7ef1a3a
CRISTIANP90 <capenam01@libertadores.edu.co>
2024-11-09 08:49:55 -0500

    domiciliosver02

--- domiciliosapp/admin.py
+++ domiciliosapp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+
 from .models import Cliente
 from .models import Pedidos
+from .models import Metodo_de_Pago
+from .models import distri_prod
+from .models import Productos
+from .models import Domiciliario
+
 
 # Register your models here.
 
 admin.site.register(Cliente)
 admin.site.register(Pedidos)
+admin.site.register(Metodo_de_Pago)
+admin.site.register(distri_prod)
+admin.site.register(Productos)
+admin.site.register(Domiciliario)
+
+
--- domiciliosapp/models.py
+++ domiciliosapp/models.py
@@ -4,12 +4,46 @@ from django.db import models
 
 class Cliente(models.Model):
     cliente_id = models.CharField(max_length=60)
-    nombre = models.CharField(max_length=60)
-    direccion = models.CharField(max_length=60)
-    telefono = models.CharField(max_length=60)
+    Nombre = models.CharField(max_length=60)
+    Direccion = models.CharField(max_length=60)
+    Ciudad = models.CharField(max_length=60)
+    Telefono = models.CharField(max_length=20)
+
 
 class Pedidos(models.Model):
     pedido_id = models.CharField(max_length=60)
-    id_cliente = models.CharField(max_length=60)
-    id_producto = models.CharField(max_length=60)
-    Id_domiciliario = models.CharField(max_length=60)
+    ID_cliente = models.CharField(max_length=60)
+    ID_producto = models.CharField(max_length=60)
+    ID_met_pago = models.CharField(max_length=60)
+    ID_domiciliario = models.CharField(max_length=60)
+    Fecha_de_Pedido = models.CharField(max_length=60)
+    Precio_de_Pedido = models.CharField(max_length=60)
+
+class Metodo_de_Pago(models.Model):
+
+    Met_pago_ID = models.CharField(max_length=60)
+    Precio_ped =  models.CharField(max_length=60)
+    Modo_de_Pago = models.CharField(max_length=60)
+    Estado_de_Pago = models.CharField(max_length=60)
+
+class distri_prod(models.Model):
+
+    Distribu_ID = models.CharField(max_length=60)
+    NombreD =  models.CharField(max_length=60)
+    Tipo_distribu = models.CharField(max_length=60)
+    Ubicacion = models.CharField(max_length=60)
+
+class Productos (models.Model):
+
+    producto_ID = models.CharField(max_length=60)
+    Nombre_pro =  models.CharField(max_length=60)
+    Cantidad_pro = models.CharField(max_length=60)
+    Precio_pro = models.CharField(max_length=60)
+    ID_distribu = models.CharField(max_length=60)
+
+class Domiciliario(models.Model):
+
+    Domiciliario_ID = models.CharField(max_length=60)
+    Nombre_dom =  models.CharField(max_length=60)
+    Telefono_Dom = models.CharField(max_length=60)
+    Disponible = models.CharField(max_length=60)
